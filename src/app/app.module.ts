0d07eb12fe26ab7cc4465d27201e8c4b054d46fd
Hayden Eastwell <heastwell@tier2consulting.com>
2020-11-25 16:55:43 +0000

    Added base component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { UserOrdersComponent } from './user-orders/user-orders.component';
+import { ManageOrdersComponent } from './manage-orders/manage-orders.component';
+import { ManageProductComponent } from './manage-product/manage-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ShoppingCartComponent,
+    UserOrdersComponent,
+    ManageOrdersComponent,
+    ManageProductComponent
   ],
   imports: [
     BrowserModule,
